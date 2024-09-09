75a9f4a9e69ca961db0a87a3fb2b2fcf8facf5b6
asamad786 <asamadyousuf1989@gmail.com>
2024-09-10 00:04:18 +0500

    Day3,4,5

--- day3.py
+++ day3.py
@@ -18,3 +18,43 @@ bmi = weight / height ** 2
 print(bmi)
 
 print("=======================================================")      
+
+
+'''
+BMI Calculator with Interpretations
+Add some if/elif/else statements to the BMI calculator so that it interprets the BMI values calculated.
+
+If the bmi is under 18.5 (not including), print out "underweight"
+
+If the bmi is between 18.5 (including) and 25 (not including), print out "normal weight"
+
+If the bmi is 25 (including) or over, print out "overweight"
+
+Refer to this graphic for help:
+
+UNDERWEIGHT:
+< 18.5
+
+NORMAL 
+18, 5 -24, 9
+
+OVERWEIGHT 
+25-29, 9
+
+'''
+weight = 85
+height = 1.85
+
+bmi = weight / (height ** 2)
+
+print ("BMI: ", bmi)
+
+if bmi < 18.5:
+    print("underweight")
+elif 18.5 <= bmi < 25:
+    print("normal weight")
+else:
+    print("overweight")
+
+
+print("=======================================================")      
